c0153ac2ae6a4c5d5e43fa829a205803d5f490d2
BuildTools <unconfigured@null.spigotmc.org>
2022-09-05 20:40:33 -0300

    update login.html

--- js/login.js
+++ js/login.js
@@ -15,25 +15,6 @@ function login() {
     }
 }
 
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    if (profile) {
-        ("ID: " + profile.getId()); // Don't send this directly to your server!
-        ('Full Name: ' + profile.getName());
-        ('Given Name: ' + profile.getGivenName());
-        ('Family Name: ' + profile.getFamilyName());
-        ("Image URL: " + profile.getImageUrl());
-        ("Email: " + profile.getEmail());
-        const usuario = { nombre: profile.getName(), estado: 'conectado', imageUrl: profile.getImageUrl(), getEmail: profile.getEmail(), };
-
-
-        localStorage.setItem('usuario', JSON.stringify(usuario));
-        sessionStorage.setItem('usuario', JSON.stringify(usuario));
-        location.href = "/index.html";
-    }
-
-}
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
