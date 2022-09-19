701ab65ae35b596e72a7833ca1dc4bf32966fcee
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-09-19 15:32:32 -0300

    entrega 4

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,11 @@
 var prodDict = {}
 var commArr = []
 
+function setProdID(id) {
+    localStorage.setItem("ProdID", id);
+    window.location = "product-info.html"
+}
+
 function agregar_comentario(comentario, puntuacion) {
     let comToAppend = "";
     let estrellas = "";
@@ -46,9 +51,10 @@ function agregar_comentario(comentario, puntuacion) {
 
 function showCategoriesList() {
     let htmlContentToAppend = "";
-    let imgContentToAppend = "";
     let comContentToAppend = "";
+    let relContentToAppend = "";
     let imagenes = prodDict.images
+    let relArr = prodDict.relatedProducts
     //aca se genera todo lo relacionado con el producto
     //tremendamente agarrado con alambres, seria ideal hacer otro contenedor para los comentarios pero bueno
     htmlContentToAppend += `
@@ -63,51 +69,46 @@ function showCategoriesList() {
                     <p>${prodDict.category}</p><br>
                     <h8><strong>Cantidad de vendidos</strong></h8>
                     <p>${prodDict.soldCount}</p><br>
-                    <h8><strong>Imágenes ilustrativas</strong></h8><br>
-                    <div class="container">
-                        <div class="row row-cols-1 row-cols-md-4 g-4" id="contenedor-imagenes"></div>
-                    </div><br>
-                    <h4>Comentarios</h4>
-                    <div class="list-group" id="cat-list-container"></div><br>
-                    <h4>Comentar</h4>
-                    <h6>Tu opinion</h6>
-                    <div class="row">
-                        <div class="col-md-12 mb-3">
-                            <label for="Description">Descripción</label>
-                            <textarea name="Description" class="form-control" id="Description"></textarea>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4 mb-3">
-                            <label for="puntuacion">Tu puntuación</label>
-                            <select name="puntuacion" class="form-select custom-select d-block w-100" id="puntuacion" required>
-                                <option value="" hidden selected>Seleccionar puntuación</option>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                            <div class="invalid-feedback">
-                                Ingresa una categoría válida.
-                            </div>
-                        </div>
-                    </div>
                     
                 `
-    document.getElementById("contenedor").innerHTML += htmlContentToAppend;
+    document.getElementById("contenedor-ppal").innerHTML += htmlContentToAppend;
     
     //se hace un for para integrar las imagenes y con el inner se pone en el lugar correspondiente
     for (let i = 0; i < imagenes.length; i++) {
         imagen = imagenes[i]
-        imgContentToAppend += `
-                        <div class="card me-md-4" style="width: 18rem;">
-                            <img class="card-img-top" src="${imagen}" alt="Card image cap">
-                        </div>
+
+        if (i == 0){
+        document.getElementById("carrusel-indicadores").innerHTML += `
+        <button type="button" data-bs-target="#contenedor-imagenes" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Slide ${i+1}"></button>
         `
+        document.getElementById("carrusel-inner").innerHTML += `
+        <div class="carousel-item active">
+            <img src=${imagen} class="d-block w-100" alt=${prodDict.name}>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>${prodDict.name}</h5>
+            </div>
+        </div>
+        `
+    
+    
+    
+        }
+        else {
+            document.getElementById("carrusel-indicadores").innerHTML += `<button type="button" data-bs-target="#contenedor-imagenes" data-bs-slide-to="${i}" aria-label="Slide ${i+1}"></button>`
+
+
+            document.getElementById("carrusel-inner").innerHTML +=  `
+            <div class="carousel-item">
+            <img src=${imagen} class="d-block w-100" alt=${prodDict.name}>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${prodDict.name}</h5>
+                </div>
+            </div>`
+        }
+
     }
 
-    document.getElementById("contenedor-imagenes").innerHTML += imgContentToAppend;
+    
 
     //console.log(commArr)
     //misma idea que con las imagenes
@@ -146,6 +147,22 @@ function showCategoriesList() {
     }
     
     document.getElementById("cat-list-container").innerHTML += comContentToAppend;
+
+
+    for (let i = 0; i < relArr.length; i++){
+        let relaciones = relArr[i];
+
+        relContentToAppend += `
+            <div OnClick="setProdID(${relaciones.id})" class="card me-md-5 cursor-active" style="width: 18rem;">
+                <img class="card-img-top" src="${relaciones.image}" alt="Card image cap">
+                    <div class="card-body overflow-auto" style="max-height: 150px;">
+                        <h5 class="card-title">${relaciones.name}</h5>
+                    </div>
+            </div>`
+        
+    }
+    
+    document.getElementById("contenedor-relaciones").innerHTML += relContentToAppend;
     
     
 }
