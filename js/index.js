2c2bccdc447d1e00957abc1545dc25eb15769068
Fernando Diego Borges Da Luz <fernando.borges@fing.edu.uy>
2022-09-19 16:07:39 -0300

    test login google

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+window.handleCredentialResponse = (response) => {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    responsePayload = decodeJwtResponse(response.credential);
+  
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+  }
+  
+
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
